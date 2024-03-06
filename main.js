eee2e7586a00fdf2d4a4a8d13fc6f0c02ab001ed
Pratik <71585947+Pratik543@users.noreply.github.com>
2024-03-06 22:12:54 +0530

    quote updated

---
+++ main.js
@@ -0,0 +1,26 @@
+import "./style.css";
+
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const quoteAnimeText = document.getElementById("anime");
+
+const apiUrl = "https://animechan.xyz/api/random";
+
+const getQuote = async (apiUrl) => {
+  try {
+    const response = await fetch(apiUrl);
+    const apiData = await response.json();
+    var apiQuote = apiData.quote;
+    var apiAuthor = apiData.character;
+    var apiAnime = apiData.anime;
+
+    quoteText.textContent = apiQuote;
+    authorText.textContent = `By: ${apiAuthor}`;
+    quoteAnimeText.textContent = `From: ${apiAnime}`;
+    // console.log(apiData);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getQuote(apiUrl);
